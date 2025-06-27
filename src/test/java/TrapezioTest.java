feeef5322410310f2a2d0cea98d108ec3649af9f
Francisco Welio <fwelio.dev@gmail.com>
2025-06-27 15:24:01 -0300

    Feat/alguns testes

--- src/test/java/TrapezioTest.java
+++ src/test/java/TrapezioTest.java
@@ -1,2 +1,30 @@
+import ElementoConcreto.Trapezio;
+import Visitantes.MaximizarVisitor;
+import org.junit.jupiter.api.Test;
+import Exception.Exceptions;
+import static org.junit.jupiter.api.Assertions.*;
 public class TrapezioTest {
+
+    @Test
+    public void testCriarTrapezioValido() throws Exceptions {
+        Trapezio t = new Trapezio(8, 4, 3, 5, 5);
+        assertEquals(8, t.getBaseMaior());
+        assertEquals(4, t.getBaseMenor());
+    }
+
+    @Test
+    public void testCriarTrapezioInvalido() {
+        assertThrows(Exceptions.class, () -> new Trapezio(-1, 4, 3, 5, 5));
+    }
+
+    @Test
+    public void testMaximizarTrapezio() throws Exceptions {
+        Trapezio t = new Trapezio(2, 1, 3, 4, 5);
+        new MaximizarVisitor().visitarTrapezio(t);
+        assertEquals(4, t.getBaseMaior());
+        assertEquals(2, t.getBaseMenor());
+        assertEquals(6, t.getAltura());
+        assertEquals(8, t.getLado1());
+        assertEquals(10, t.getLado2());
+    }
 }
--- src/test/java/TrianguloTest.java
+++ src/test/java/TrianguloTest.java
@@ -1,2 +1,29 @@
+import ElementoConcreto.Triangulo;
+import Visitantes.MaximizarVisitor;
+import org.junit.jupiter.api.Test;
+import Exception.Exceptions;
+import static org.junit.jupiter.api.Assertions.*;
 public class TrianguloTest {
+
+    @Test
+    public void testCriarTrianguloValido() throws Exceptions {
+        Triangulo t = new Triangulo(3, 4, 5, 5);
+        assertEquals(3, t.getBase());
+        assertEquals(4, t.getAltura());
+    }
+
+    @Test
+    public void testCriarTrianguloInvalido() {
+        assertThrows(Exceptions.class, () -> new Triangulo(0, 4, 5, 5));
+    }
+
+    @Test
+    public void testMaximizarTriangulo() throws Exceptions {
+        Triangulo t = new Triangulo(2, 3, 4, 5);
+        new MaximizarVisitor().visitarTriangulo(t);
+        assertEquals(4, t.getBase());
+        assertEquals(6, t.getAltura());
+        assertEquals(8, t.getLado1());
+        assertEquals(10, t.getLado2());
+    }
 }
